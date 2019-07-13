fcecf989f4cc7776e113fcdd0339a26975f1fa60
garyCC227 <z5163479@ad.unsw.edu.au>
2019-07-13 22:56:12 +1000

    s

--- lab/Ex04/Ex04.hs
+++ lab/Ex04/Ex04.hs
@@ -62,22 +62,61 @@ instance Monad Calc where
 -- x :: Token = Number Int | operator
 -- Calc Int :: C (\xs -> (xs, Int))
 evaluate :: [Token] -> Calc Int
-evaluate [] = pop
-evaluate (Number i: ts) = 
-  do
-    push i
-    evaluate ts
-evaluate (t:ts) = 
-  do
-    evaluate ts
- 
+-- evaluate [] = pop
+-- evaluate (Number i: ts) = 
+--   do
+--     push i
+--     evaluate ts
+-- evaluate (t:ts) = 
+--   do
+--     evaluate ts
+evaluate ts = f [] False ts
+  where
+    f:: [(Int -> Int -> Int)] -> Bool -> [Token] -> Calc Int
+    f ops isPending [] =
+        case ops of
+          [] ->
+            do
+              pop
+          (op:ops') ->
+            do
+              operand_1 <- pop
+              operand_2 <- pop
+              push (operand_1 `op` operand_2) -- TODO
+              f ops' isPending []
+
+    f ops isPending ((Number i):ts) = 
+      do
+        case ops of 
+          []        -> do 
+            -- if isPending == True, then we dont have a operator, then return Nothing
+              C $ (\xs -> Nothing)
+          (op:ops') ->
+            do
+              if isPending then do
+                operand <- pop
+                push (i `op` operand)
+                f ops' isPending ts
+              else 
+                do
+                  push i
+                  f (op:ops') True ts
+
+    f ops isPending ((Operator op): ts) = 
+      do
+        (f (pushOP op ops) False ts)
+
+-- pushOp :: 
+pushOP op [] =[op]
+pushOP op xs = op:xs
+
 
 calculate :: String -> Maybe Int
 calculate s = f (tokenise s)
   where
     f :: Maybe [Token] -> Maybe Int
     f Nothing = Nothing
-    f (Just ts) = helper (calcHelp ts)
+    f (Just ts) = helper (evaluate ts)
       where
         helper :: Calc Int -> Maybe Int
         helper (C intF) = helper2 (intF [])
@@ -122,6 +161,6 @@ calcHelp ts = f [] False ts
       do
         (f (pushOP op ops) False ts)
 
--- pushOp :: 
-pushOP op [] =[op]
-pushOP op xs = op:xs
+-- -- pushOp :: 
+-- pushOP op [] =[op]
+-- pushOP op xs = op:xs
